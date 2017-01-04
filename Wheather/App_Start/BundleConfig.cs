4a10d8284457412a20faeab87b145ef0cdda02d1
ayalayan <ayalag@grts.co.il>
2017-01-04 09:25:14 +0200

    commit

--- Wheather/App_Start/BundleConfig.cs
+++ Wheather/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace Weather
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+            "~/Scripts/angular*.js"));
         }
     }
 }
--- Wheather/app/home/home.ctrl.js
+++ Wheather/app/home/home.ctrl.js
@@ -18,7 +18,8 @@
        *****************************************************/
         $scope.$watch(function () { return vm.cityName; }, function (newVal, oldVal) {
             vm.resultSearchMessage = null;
-            if (newVal && newVal.length > 1) {
+            var isExists= newVal && newVal.length > 1;
+            if (isExists) {
                 vm.cities = wtrCitiesData.searchCitiesByKey(newVal);
                 if (vm.cities == null || vm.cities == undefined || vm.cities.length < 1) {
                     vm.resultSearchMessage = 'No cities found';
